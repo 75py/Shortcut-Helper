5a4df15dea08af3b6e1e4c03686124df4e158dc1
75py <dev.75py@gmail.com>
2013-01-02 22:19:56 +0900

    v1.1.0 beta

---
+++ src/com/nagopy/shortcut/helper/dialog/ListDialogFragment.java
@@ -0,0 +1,77 @@
+package com.nagopy.shortcut.helper.dialog;
+
+import java.io.Serializable;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListDialogFragment extends DialogFragment {
+
+	/**
+	 * 配列のリソースIDを保存するためのキー
+	 */
+	private static final String KEY_ARRAYSTRING_RES_ID = "KEY_ARRAYSTRING_RES_ID";
+
+	/**
+	 * ダイアログのタイトルを保存するためのキー
+	 */
+	private static final String KEY_DIALOG_TITLE = "KEY_DIALOG_TITLE";
+
+	/**
+	 * リスナーを保存するためのキー
+	 */
+	private static final String KEY_ON_ITEM_CLICK_LISTENER = "KEY_ON_ITEM_CLICK_LISTENER";
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		ListView listView = new ListView(getActivity().getApplicationContext());
+		String[] array = getActivity().getResources().getStringArray(getArrayResId());
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, array);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(getOnItemClickListener());
+
+		return new AlertDialog.Builder(getActivity()).setView(listView)
+				.setTitle(getDialogTitle()).create();
+	}
+
+	/**
+	 * 保存した配列のリソースIDを取得する
+	 */
+	private int getArrayResId() {
+		return getArguments().getInt(KEY_ARRAYSTRING_RES_ID);
+	}
+
+	/**
+	 * 保存したリスナーを取得する
+	 */
+	private OnItemClickListener getOnItemClickListener() {
+		return (OnItemClickListener) getArguments().get(KEY_ON_ITEM_CLICK_LISTENER);
+	}
+	
+	private String getDialogTitle(){
+		return getArguments().getString(KEY_DIALOG_TITLE);
+	}
+
+	/**
+	 * 配列のリソースIDとリスナーを設定する
+	 * @param arrayResId
+	 * @param listener
+	 */
+	public void init(String title, int arrayResId, OnListDialogItemClickListener listener) {
+		Bundle bundle = new Bundle();
+		bundle.putString(KEY_DIALOG_TITLE, title);
+		bundle.putInt(KEY_ARRAYSTRING_RES_ID, arrayResId);
+		bundle.putSerializable(KEY_ON_ITEM_CLICK_LISTENER, listener);
+		setArguments(bundle);
+	}
+
+	@SuppressWarnings("serial")
+	public static abstract class OnListDialogItemClickListener implements OnItemClickListener, Serializable {
+	}
+}
